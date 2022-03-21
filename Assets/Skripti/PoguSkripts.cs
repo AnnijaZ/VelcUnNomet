0ebd26012f3e5c03d75a457901aa2e81cc9425f3
Zorgenberga <annija.zorgenberga@gmail.com>
2022-03-21 14:57:25 +0200

    Izveidots izvelnes logs un nodrosinata ainas parslegsanas saksanai

---
+++ Assets/Skripti/PoguSkripts.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PoguSkripts : MonoBehaviour {
+	//Nodrosina ainu parslegsanu, uzkliksinot uz pogu parsledzas ainas
+	public void uzkliksinot (){
+		SceneManager.LoadScene("Pilseta");
+	}
+}
